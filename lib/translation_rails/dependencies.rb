79a92ea2ecb74d451675d73d54297689cc2f5577
Jio Lee <jypandjio@163.com>
2013-06-05 16:02:32 +0800

    Fixed: dependencies

--- lib/translation_rails/dependencies.rb
+++ lib/translation_rails/dependencies.rb
@@ -13,7 +13,7 @@ module ActiveSupport
         require_or_load_without_translation_rails(file_name, const_path)
         if file_name.starts_with?(Rails.root.to_s + '/app')
           relative_name = file_name.gsub(Rails.root.to_s, '')
-          engine_file = File.join(TranslationRails::Engine.root, relative_name)
+          engine_file = File.join(::TranslationRails::Engine.root, relative_name)
           require_or_load_without_translation_rails(engine_file, const_path) if File.file?(engine_file)
         end
       end
